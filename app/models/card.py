cbdf77198ea4812c36a16fadd5b95b0a15a3386e
peacegypsy <christinasherman206@gmail.com>
2021-07-20 17:59:49 -0700

    removing weird stuff that was added

--- app/models/card.py
+++ app/models/card.py
@@ -3,8 +3,8 @@ from app import db
 
 class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True)
-    card_title = db.Column(db.String(100), nullable=False)
+    card_name = db.Column(db.String(100), nullable=False)
     card_general = db.Column(db.Text, nullable=False)
     card_upright = db.Column(db.Text, nullable=False)
     card_reversed = db.Column(db.Text, nullable=False)
-    card_image = db.Column(db.Integer, db.ForeignKey("image.image_id"))
+    card_image_location = db.Column(db.String)
